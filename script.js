606153ad8793c5ac821577833ac7b9e12d55d42a
Timmy Chen <chenx343@gmail.com>
2023-05-22 22:51:20 -0400

    changed window to document

--- script.js
+++ script.js
@@ -51,6 +51,6 @@ projects.forEach((p) => {
 });
 
 window.addEventListener("resize", checkNav)
-window.addEventListener("DOMContentLoaded", checkNav);
+document.addEventListener("DOMContentLoaded", checkNav);
 
 
