ea8c99e4ff55f662eeec3f8ecf59189405b44d29
Timmy Chen <chenx343@gmail.com>
2024-02-23 14:32:11 -0500

    make changes to js file to update resume link

--- script.js
+++ script.js
@@ -3,51 +3,48 @@ const projectGrid = document.querySelector(".projects-grid");
 const projects = projectGrid.querySelectorAll(".project");
 
 function scrollToTop(e) {
-    if (e.target.classList.contains("nav-name")){
-        window.scrollTo(0, 0);
-        console.log("hello");
-    }
+  if (e.target.classList.contains("nav-name")) {
+    window.scrollTo(0, 0);
+    console.log("hello");
+  }
 }
 
 function transition(e) {
-    e.target.querySelector(".project-text").style.display = "flex";
-    e.target.querySelector(".project-cover").style.backgroundColor = "rgba(202, 129, 56, 0.85)";
-    e.target.querySelector(".project-cover").style.transition = ".3s";
+  e.target.querySelector(".project-text").style.display = "flex";
+  e.target.querySelector(".project-cover").style.backgroundColor = "rgba(202, 129, 56, 0.85)";
+  e.target.querySelector(".project-cover").style.transition = ".3s";
 }
 
 function reset(e) {
-    e.target.querySelector(".project-text").style.display = "none";
-    e.target.querySelector(".project-cover").style.backgroundColor = "rgba(202, 129, 56, 0)";
+  e.target.querySelector(".project-text").style.display = "none";
+  e.target.querySelector(".project-cover").style.backgroundColor = "rgba(202, 129, 56, 0)";
 }
 
 function checkNav(e) {
-    const navBar = document.querySelector(".nav-flex");
-    if (window.innerWidth < 1200) {
-        navBar.innerHTML = `<div class = "nav-name">Timmy Chen</div>
+  const navBar = document.querySelector(".nav-flex");
+  if (window.innerWidth < 1200) {
+    navBar.innerHTML = `<div class = "nav-name">Timmy Chen</div>
             <a class = "nav-link" href="Resume/current-resume-timmy-chen-copy.pdf" target = "_blank" rel = "noopener noreferrer">Resume</a>
             <a class = "nav-link" href="https://github.com/chimmyten" target = "_blank" rel = "noopener noreferrer">Github</a>
             <a class = "nav-link" href="https://www.linkedin.com/in/timmychen1/" target = "_blank" rel = "noopener noreferrer">LinkedIn</a>`;
-        navBar.classList.add("nav-small-flex");
-    }
-    else if (window.innerWidth > 1200){
-        navBar.classList.remove("nav-small-flex")
-        navBar.innerHTML = `<div class = "nav-name">Timmy Chen</div>
+    navBar.classList.add("nav-small-flex");
+  } else if (window.innerWidth > 1200) {
+    navBar.classList.remove("nav-small-flex");
+    navBar.innerHTML = `<div class = "nav-name">Timmy Chen</div>
             <div class = "nav-content-flex">
-            <a class = "nav-link" href="Resume/current-resume-timmy-chen-copy.pdf" target = "_blank" rel = "noopener noreferrer">Resume</a>
+            <a class = "nav-link" href="Resume/Current_Resume_Timmy_Chen_copy.pdf" target = "_blank" rel = "noopener noreferrer">Resume</a>
             <a class = "nav-link" href="https://github.com/chimmyten" target = "_blank" rel = "noopener noreferrer">Github</a>
             <a class = "nav-link" href="https://www.linkedin.com/in/timmychen1/" target = "_blank" rel = "noopener noreferrer">LinkedIn</a>
-        <div>`; 
-    }
-
+        <div>`;
+  }
 }
 checkNav();
 
 document.querySelector(".nav-flex").addEventListener("click", scrollToTop);
 
 projects.forEach((p) => {
-	p.addEventListener("mouseover", transition);
-    p.addEventListener("mouseout", reset)
+  p.addEventListener("mouseover", transition);
+  p.addEventListener("mouseout", reset);
 });
 
-window.addEventListener("resize", checkNav)
-
+window.addEventListener("resize", checkNav);
